1488019bbbf0f9d582fa6a5ec8b2fae8ccb66b10
Kyle Tulod <tulod.kyle.m@gmail.com>
2016-12-15 16:36:08 -0500

    done

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_data = params
+    @full_param_names = {"name" => "Team Name",
+      "coach" => "Coach",
+      "pg" => "Point Guard",
+      "sg" => "Shooting Guard",
+      "pf" => "Power Forward",
+      "sf" => "Small Forward",
+      "c" => "Center"
+    }
+    erb :team
+  end
 
 end
